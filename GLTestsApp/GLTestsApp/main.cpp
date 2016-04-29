75f1d1db860c59002ba0ae4451c595f1b4c9b2c2
Catalin <catalin.ciobirca@yahoo.com>
2016-04-29 14:54:00 +0300

    test with OpenGL Core Profile.

--- GLTestsApp/GLTestsApp/main.cpp
+++ GLTestsApp/GLTestsApp/main.cpp
@@ -1,6 +1,9 @@
 #include "GLTestsApp.h"
 #include <QApplication>
 #include <QDebug>
+//#ifdef __APPLE__
+//#include <QSurfaceFormat>
+//#endif
 
 #include "AppObjLoader/3d_model.h"
 #include "AppObjLoader/obj_loader.h"
@@ -12,6 +15,17 @@ std::string AppResPath = APPRESPATH;
 
 int main(int argc, char *argv[])
 {
+//#ifdef __APPLE__
+//    QSurfaceFormat surfaceFormat;
+//    surfaceFormat.setVersion(4, 1);
+//    surfaceFormat.setProfile(QSurfaceFormat::CoreProfile);
+//    //surfaceFormat.setSamples(4);
+//    surfaceFormat.setRenderableType(QSurfaceFormat::OpenGL);
+//    //surfaceFormat.setSwapBehavior(QSurfaceFormat::TripleBuffer);
+//    surfaceFormat.setSwapBehavior(QSurfaceFormat::DoubleBuffer);
+//    QSurfaceFormat::setDefaultFormat(surfaceFormat);
+//#endif
+
     QApplication a(argc, argv);
     qDebug() << AppResPath.c_str();
     //a.setStyle("fusion");
